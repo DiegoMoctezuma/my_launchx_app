914bdf0cc85e72049f06e4d30b696aa6392393ef
Rafael Moctezuma <moctezumarafael26@gmail.com>
2022-04-27 19:31:29 -0500

    pruebas de unidad

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require("./app/missionCommander");
+
+const fer8a = new MissionCommander("Fernanda");
+const romarpla = new MissionCommander("Rodrigo");
+const carlo = new MissionCommander("Carlo");
+
+console.log(fer8a.name);
+console.log(romarpla.name);
+console.log(carlo.name);
